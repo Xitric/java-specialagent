3b328ce543f6ac48d1adb4bdbfd23de8a75c5767
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2021-03-12 16:29:06 +0100

    Cleaned up OSGI classloader

--- opentracing-specialagent/src/main/java/io/opentracing/contrib/specialagent/OsgiClassLoaderAgent.java
+++ opentracing-specialagent/src/main/java/io/opentracing/contrib/specialagent/OsgiClassLoaderAgent.java
@@ -4,237 +4,100 @@ import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.asm.TypeConstantAdjustment;
 import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.dynamic.DynamicType;
-import net.bytebuddy.implementation.bytecode.assign.Assigner;
+import net.bytebuddy.implementation.bytecode.assign.Assigner.Typing;
 import net.bytebuddy.matcher.ElementMatchers;
 import net.bytebuddy.utility.JavaModule;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
-import java.util.ArrayList;
-import java.util.Collections;
 
 import static io.opentracing.contrib.specialagent.DefaultAgentRule.log;
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
 /**
+ * An additional classloading injection performed specifically for OSGi classloaders, since they work a little different
+ * from others. This has caused missing class injections, and as a result many ClassNotFoundExceptions at runtime.
+ *
  * @author Kasper
  */
 public class OsgiClassLoaderAgent {
-	public static final ClassFileLocator locatorProxy = BootLoaderAgent.cachedLocator;
 
-	//	private static final AgentBuilder.LocationStrategy bootFallbackLocationStrategy = new AgentBuilder.LocationStrategy() {
-	//		@Override
-	//		public ClassFileLocator classFileLocator(final ClassLoader classLoader, final JavaModule module) {
-	//			return new ClassFileLocator.Compound(
-	//					ClassFileLocator.ForClassLoader.ofBootLoader(),
-	//					ClassFileLocator.ForClassLoader.of(classLoader)
-	//			);
-	//		}
-	//	};
-	//
-	//	public static AgentBuilder premain(final AgentBuilder builder) {
-	//		log("\n<<<<<<<<<<<<<<<<< Installing OsgiClassLoaderAgent >>>>>>>>>>>>>>>>>>\n", null, DefaultAgentRule.DefaultLevel.FINE);
-	//		try {
-	//			return builder
-	//					.with(bootFallbackLocationStrategy)
-	//					.type(isSubTypeOf(ClassLoader.class))
-	//					.and(ElementMatchers.<TypeDescription>nameContains("osgi"))
-	//					.transform(new AgentBuilder.Transformer() {
-	//						           @Override
-	//						           public DynamicType.Builder<?> transform(DynamicType.Builder<?> builder, TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule) {
-	//							           return builder.visit();
-	//						           }
-	//					           }
-	//					);
-	//		} finally {
-	//			log("\n>>>>>>>>>>>>>>>>>> Installed OsgiClassLoaderAgent <<<<<<<<<<<<<<<<<<\n", null, DefaultAgentRule.DefaultLevel.FINE);
-	//		}
-	//	}
+	private static final String CLASS_LOADER_NAME = "osgi";
+	private static final String SPECIAL_AGENT_NAMESPACE = "io.opentracing.contrib.specialagent.rule";
 
 	public static AgentBuilder premain(final AgentBuilder builder) {
 		log("\n<<<<<<<<<<<<<<<<< Installing OsgiClassLoaderAgent >>>>>>>>>>>>>>>>>>\n", null, DefaultAgentRule.DefaultLevel.FINE);
 		try {
-			return builder.type(isSubTypeOf(ClassLoader.class)).and(ElementMatchers.<TypeDescription>nameContains("osgi")).transform(new AgentBuilder.Transformer() {
-				@Override
-				public DynamicType.Builder<?> transform(final DynamicType.Builder<?> builder, final TypeDescription typeDescription, final ClassLoader classLoader, final JavaModule module) {
-					return builder
-							.visit(TypeConstantAdjustment.INSTANCE)
-//							.visit(Advice.to(LoadClassAdvice.class).on(named("findClass").and(returns(Class.class).and(takesArgument(0, String.class)))));
-							.visit(Advice.to(LoadClassAdvice.class).on(isMethod()
-									.and(named("loadClass"))
-									.and(
-											takesArguments(1)
-													.and(takesArgument(0, named("java.lang.String")))
-													.or(
-															takesArguments(2)
-																	.and(takesArgument(0, named("java.lang.String")))
-																	.and(takesArgument(1, named("boolean")))))
-									.and(isPublic().or(isProtected()))
-									.and(not(isStatic()))));
-				}});
-//			return builder.transform(
-//					new AgentBuilder.Transformer.ForAdvice()
-//							.include(BootProxyClassLoader.INSTANCE, SpecialAgent.class.getClassLoader())
-//							.advice(
-//									named("findClass").and(returns(Class.class).and(takesArgument(0, String.class))),
-//									OsgiClassLoaderAgent.class.getName() + "$LoadClassAdvice"
-//							)
-//			);
-		}
-		finally {
+			return builder.type(isSubTypeOf(ClassLoader.class))
+					.and(ElementMatchers.<TypeDescription>nameContains(CLASS_LOADER_NAME))
+					.transform(new AgentBuilder.Transformer() {
+						@Override
+						public DynamicType.Builder<?> transform(
+								final DynamicType.Builder<?> builder,
+								final TypeDescription typeDescription,
+								final ClassLoader classLoader,
+								final JavaModule module
+						) {
+							return builder
+									.visit(TypeConstantAdjustment.INSTANCE)
+									.visit(Advice.to(LoadClass.class).on(
+											named("loadClass").and(
+													takesArguments(2)
+															.and(takesArgument(0, String.class))
+															.and(takesArgument(1, Boolean.class)))
+													.and(returns(Class.class).and(isPublic().or(isProtected())))
+											)
+									);
+						}
+					});
+		} finally {
 			log("\n>>>>>>>>>>>>>>>>>> Installed OsgiClassLoaderAgent <<<<<<<<<<<<<<<<<<\n", null, DefaultAgentRule.DefaultLevel.FINE);
 		}
 	}
 
-	public static class LoadClassAdvice {
-		// @Advice.OnMethodEnter(skipOn = Advice.OnNonDefaultValue.class)
-		// public static Class<?> onEnter(@Advice.Argument(0) String name, @Advice.This ClassLoader injectedOn) {
-			// ClassLoaderLogger.log(injectedOn, "Enter LoadClass for " + name);
-			// ClassLoaderLogger.dumpHierarchy(injectedOn);
-			// need to use call depth here to prevent re-entry from call to Class.forName() below
-			// because on some JVMs (e.g. IBM's, though IBM bootstrap loader is explicitly excluded above)
-			// Class.forName() ends up calling loadClass() on the bootstrap loader which would then come
-			// back to this instrumentation over and over, causing a StackOverflowError
-			// TODO: Not a problem for me? ;)
-			// int callDepth = CallDepthThreadLocalMap.incrementCallDepth(ClassLoader.class);
-			// if (callDepth > 0) {
-			// 	return null;
-			// }
-
-//			if (true) {
-//				throw new RuntimeException("Okay, we actually injected something. Cool!");
-//			}
-
-// 			try (PrintWriter writer = new PrintWriter(new FileWriter("C:\\Users\\Kasper\\Desktop\\loaderlog.txt", true))) {
-
-// 				writer.println("Trying to load: " + name);
-
-// 				try {
-// 					//				for (String prefix : Holder.bootstrapPackagesPrefixes) {
-// 					if (name.startsWith("io.opentracing")) {
-// // 						writer.println("Thread is " + Thread.currentThread());
-// // 						ClassLoaderLogger.log(injectedOn, "Thread is " + Thread.currentThread());
-
-// // 						try {
-// // 							Class a = Class.forName(name, false, null);
-// // 							writer.println("Boot loader found: " + a);
-// // 						} catch (ClassNotFoundException ignored) {
-// // 							writer.println("Boot loader failed");
-// // 						}
-
-// // 						ClassLoaderLogger.log(injectedOn, "Determining hierarchy for bootproxy");
-// // 						ClassLoaderLogger.dumpHierarchy(BootProxyClassLoader.INSTANCE);
-
-// // 						try {
-// // 							Class b = Class.forName(name, false, BootProxyClassLoader.INSTANCE);
-// // 							writer.println(BootProxyClassLoader.INSTANCE + " found: " + b);
-// // 						} catch (ClassNotFoundException ignored) {
-// // 							writer.println(BootProxyClassLoader.INSTANCE + " failed");
-// // 						}
-
-// // 						ClassLoaderLogger.log(injectedOn, "Determining hierarchy for SpecialAgent");
-// // 						ClassLoaderLogger.dumpHierarchy(SpecialAgent.class.getClassLoader());
-
-// // 						try {
-// // 							Class c = Class.forName(name, false, SpecialAgent.class.getClassLoader());
-// // 							writer.println(SpecialAgent.class.getClassLoader() + " found: " + c);
-// // 						} catch (ClassNotFoundException ignored) {
-// // 							writer.println(SpecialAgent.class.getClassLoader() + " failed");
-// // 						}
-
-// // 						Class d = BootProxyClassLoader.INSTANCE.loadClassOrNull(name, false);
-// // 						writer.println(BootProxyClassLoader.INSTANCE + " found by special means: " + d);
-
-// // 						try {
-// // 							Class e = Class.forName(name, false, injectedOn.getParent());
-// // 							writer.println("Parent classloader found: " + e);
-// // 						} catch (ClassNotFoundException ignored) {
-// // 							writer.println("Parent classloader failed");
-// // 						}
-
-// // 						ClassLoaderLogger.log(injectedOn, "Determining hierarchy for ContextClassLoader");
-// // 						ClassLoaderLogger.dumpHierarchy(Thread.currentThread().getContextClassLoader());
-
-// 						// try {
-// 						// 	Class e = Class.forName(name, false, Thread.currentThread().getContextClassLoader());
-// 						// 	writer.println(Thread.currentThread().getContextClassLoader() + " found: " + e);
-
-//                         //     ClassLoaderLogger.log(injectedOn, "ContextClassLoader found " + e);
-// 						// } catch (ClassNotFoundException ignored) {
-// 						// 	writer.println(Thread.currentThread().getContextClassLoader() + " failed");
-// 						// }
-
-// // //						if (d == null) {
-// // 							writer.println("Injected on: " + injectedOn + ", " + (injectedOn != null ? injectedOn.getClass().getName() + "@" + Integer.toString(System.identityHashCode(injectedOn), 16) : "null"));
-// // 							writer.println("Dump:");
-// // 							writer.println("Parent: " + injectedOn.getParent());
-// // 							writer.println("Resource: " + injectedOn.getResource(name));
-// // 							writer.println("Resources: " + Collections.list(injectedOn.getResources("io.opentracing.contrib")));
-// // 							writer.print("----------------");
-// // //						}
-						
-//                         // try {
-// 						// 	return Class.forName(name, false, Thread.currentThread().getContextClassLoader());
-// 						// } catch (ClassNotFoundException ignored) {
-// 						// 	writer.println("Failed altogether?");
-// 						// }
-// 					}
-// 					//				}
-// 				} finally {
-// 					// need to reset it right away, not waiting until onExit()
-// 					// otherwise it will prevent this instrumentation from being applied when loadClass()
-// 					// ends up calling a ClassFileTransformer which ends up calling loadClass() further down the
-// 					// stack on one of our bootstrap packages (since the call depth check would then suppress
-// 					// the nested loadClass instrumentation)
-// 					CallDepthThreadLocalMap.reset(ClassLoader.class);
-// 				}
-// 			} catch (IOException e) {
-// 				e.printStackTrace();
-// 			}
-
-			// ClassLoaderLogger.log(injectedOn, "Could not find " + name);
-		// 	return null;
-		// }
-
+	public static class LoadClass {
 		public static Method defineClass;
 
-		@Advice.OnMethodExit(onThrowable = Throwable.class)
+		@Advice.OnMethodExit(onThrowable = ClassNotFoundException.class)
 		public static void onExit(
-				@Advice.Return(readOnly = false) Class<?> result,
-				// @Advice.Enter Class<?> resultFromBootstrapLoader,
-				@Advice.This ClassLoader injectedOn,
-				@Advice.Argument(0) String name,
-				@Advice.Thrown(readOnly = false, typing = Assigner.Typing.DYNAMIC) ClassNotFoundException thrown) {
-			ClassLoaderLogger.log(injectedOn, "Exit LoadClass for " + name);
-			// ClassLoaderLogger.log(injectedOn, "Result from fallback was " + resultFromBootstrapLoader);
-			// if (resultFromBootstrapLoader != null) {
-			// 	result = resultFromBootstrapLoader;
-			// } else {
-                // TODO: Should probably check bootstrap as well, and can limit scope to io.opentracing
-                // Should also limit the other one to io.opentracing packages, for optimization
+				final @Advice.This ClassLoader thiz,
+				final @Advice.Argument(0) String name,
+				final @Advice.Argument(1) Boolean resolve,
+				@Advice.Return(readOnly = false, typing = Typing.DYNAMIC) Class<?> result,
+				@Advice.Thrown(readOnly = false, typing = Typing.DYNAMIC) ClassNotFoundException thrown) {
 
-				try {
-					final byte[] bytecode = SpecialAgent.findClass(injectedOn, name);
-					if (bytecode == null) {
-						ClassLoaderLogger.log(injectedOn, "Even SpecialAgent failed, fuck?!");
-						return;
-					}
+			// Ideally, we should only be messing with class lookups from within the namespace of SpecialAgent
+			if (!name.startsWith(SPECIAL_AGENT_NAMESPACE)) {
+				return;
+			}
 
+			// Some classloaders do not propagate to their parent, so we attempt to resolve the lookup using the boot
+			// loader
+			try {
+				result = Class.forName(name, resolve, null);
+				thrown = null;
+				return;
+			} catch (ClassNotFoundException ignored) {
+			}
+
+			// Finally, we can resort to SpecialAgent, since at this point we are either missing a deferred injection or
+			// something else is very wrong
+			try {
+				final byte[] bytecode = SpecialAgent.findClass(thiz, name);
+				if (bytecode != null) {
 					if (defineClass == null)
 						defineClass = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class, int.class, int.class, ProtectionDomain.class);
 
-					result = (Class<?>)defineClass.invoke(injectedOn, name, bytecode, 0, bytecode.length, null);
-					ClassLoaderLogger.log(injectedOn, "Fallback to DefineClass, found " + result);
+					result = (Class<?>) defineClass.invoke(thiz, name, bytecode, 0, bytecode.length, null);
 					thrown = null;
-				} catch (Throwable t) {
-					ClassLoaderLogger.log(injectedOn, "Oh, something terrible happened while invoking SpecialAgent for bytecode");
 				}
-			// }
+			} catch (NoSuchMethodException | InvocationTargetException e) {
+				log("<><><><> OsgiClassLoaderAgent.LoadClass#exit", e, DefaultAgentRule.DefaultLevel.SEVERE);
+			} catch (IllegalAccessException e) {
+				log("<><><><> OsgiClassLoaderAgent.LoadClass#exit may have encountered a JVM bug", e, DefaultAgentRule.DefaultLevel.SEVERE);
+			}
 		}
 	}
 }
